3db5e9392a44887598f8d497387583ef12bc1e97
Rickylin888 <87745621+Rickylin888@users.noreply.github.com>
2023-10-15 15:20:04 -0400

    Update trafficlight2.py

--- trafficlight2.py
+++ trafficlight2.py
@@ -2,37 +2,43 @@ import tkinter as tk
 
 def update_lights():
     colors = ["red", "yellow", "green"]
-    for i, color in enumerate(colors):
-        canvas.itemconfig(north_light[i], fill=color if i == light_state else "gray")
-        canvas.itemconfig(south_light[i], fill=color if i == light_state else "gray")
-        canvas.itemconfig(east_light[i], fill=color if i == east_west_light_state else "gray")
-        canvas.itemconfig(west_light[i], fill=color if i == east_west_light_state else "gray")
-    
+    canvas.itemconfig(north_light, fill=colors[light_state])
+    canvas.itemconfig(south_light, fill=colors[light_state])
+    canvas.itemconfig(east_light, fill=colors[east_west_light_state])
+    canvas.itemconfig(west_light, fill=colors[east_west_light_state])
+
     if light_state == 0 and east_west_light_state == 0:
         canvas.itemconfig(left_turn_light, fill="green")
     else:
-        canvas.itemconfig(left_turn_light, fill="gray")
+        canvas.itemconfig(left_turn_light, fill="red")
     
-    root.after(2000, change_lights)
+    if light_state == 1 or east_west_light_state == 1:
+        # Yellow light: Change after 2 seconds
+        root.after(2000, change_lights)
+    elif light_state == 0 or east_west_light_state == 0:
+        # Red light: Change after 7 seconds
+        root.after(7000, change_lights)
+    else:
+        # Green light: Change after 5 seconds
+        root.after(5000, change_lights)
 
 def change_lights():
     global light_state, east_west_light_state
+    
     if light_state == 1: 
         light_state = 0
     elif light_state == 0: 
-        if east_west_light_state == 0:  
-            light_state = 2
+        light_state = 2 if east_west_light_state == 0 else 0
     else: 
         light_state = 1
     
     if east_west_light_state == 1: 
         east_west_light_state = 0
-    elif east_west_light_state == 0:
-        if light_state == 0:
-            east_west_light_state = 2
-    else:
+    elif east_west_light_state == 0: 
+        east_west_light_state = 2 if light_state == 0 else 0
+    else: 
         east_west_light_state = 1
-    
+
     update_lights()
 
 root = tk.Tk()
@@ -41,11 +47,7 @@ canvas = tk.Canvas(root, width=400, height=400, bg="white")
 canvas.pack()
 
 def draw_light(x, y):
-    light = []
-    colors = ["red", "yellow", "green"]
-    for i, color in enumerate(colors):
-        light.append(canvas.create_oval(x, y + i*40, x+30, y + i*40 + 30, fill="gray"))
-    return light
+    return canvas.create_oval(x, y, x+30, y + 30, fill="gray")
 
 light_positions = [(150, 10), (310, 150), (150, 290), (10, 150)]
 
@@ -54,11 +56,13 @@ south_light = draw_light(*light_positions[2])
 east_light = draw_light(*light_positions[1])
 west_light = draw_light(*light_positions[3])
 
-# Draw a left-turn light
 left_turn_light = canvas.create_oval(185, 185, 215, 215, fill="gray")
+text = canvas.create_text(200, 195, text="←", font=("Arial", 20))
 
-light_state = 0  # 0: red, 1: yellow, 2: green for N/S
-east_west_light_state = 2  # for E/W
-update_lights()
+light_state = 0  
+east_west_light_state = 2  
+
+change_lights()
 
 root.mainloop()
+
